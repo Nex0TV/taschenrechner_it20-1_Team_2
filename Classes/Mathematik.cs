14697d7771942f32329922122ee126a0503a554f
Gröll <groellju>
2022-12-14 09:19:36 +0100

    added Mathematik class and Fakultät method with unit tests

---
+++ Classes/Mathematik.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.Classes
+{
+    public class Mathematik
+    {
+        public static int Fakultaet (int zahl)
+        {
+            if(zahl == 0)
+            {
+                return 1;
+            } 
+            else
+            {
+                int ergebnis = 1;
+
+                for (int iterator = 1; iterator <= zahl; iterator++)
+                {
+                    ergebnis *= iterator;
+                }
+
+                return ergebnis;
+            }
+        }
+    }
+}
