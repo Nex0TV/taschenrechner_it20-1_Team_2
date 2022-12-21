c6b79dd1d10cb2a28caf317d7794d477c1caf4a1
testelda <testelda@PC-22-16>
2022-12-21 08:29:30 +0100

    Prozentsatz fix + Unittests Prozentsatz

--- TaschenrechnerUnitTests/Prozentrechnung/Prozentsatz.cs
+++ TaschenrechnerUnitTests/Prozentrechnung/Prozentsatz.cs
@@ -14,19 +14,22 @@ namespace TaschenrechnerUnitTests
         public void Input_0()
         {
             double result = Prozentrechnung.Prozentsatz(0,0);
-            Assert.That(result == 1, "Ergibt nicht 1!" + result + " wurde berechnet!");
+            string resultstr = result.ToString("N2");
+            Assert.That(resultstr == "1,00", "Ergibt nicht 1,00!" + resultstr + " wurde berechnet!");
         }
         [Test]
         public void Input_750()
         {
             double result = Prozentrechnung.Prozentsatz(750,5000);
-            Assert.That(result == 15, "Ergibt nicht 15! " + result + " wurde berechnet!");
+            string resultstr = result.ToString("N2");
+            Assert.That(resultstr == "15,00", "Ergibt nicht 15,00! " + resultstr + " wurde berechnet!");
         }
         [Test]
         public void Input_1250()
         {
             double result = Prozentrechnung.Prozentsatz(1250, 7777);
-            Assert.That(result == 16.07, "Ergibt nicht 16.07! " + result + " wurde berechnet!");
+            string resultstr = result.ToString("N2");
+            Assert.That(resultstr == "16,07", "Ergibt nicht 16,07! " + resultstr + " wurde berechnet!");
         }
     }
 }
