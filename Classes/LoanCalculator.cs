94e517d971bdcf03d4656c689d7444939279ab3f
Dang <dangvi>
2022-12-21 08:39:07 +0100

    changes kreditrechner

---
+++ Classes/LoanCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taschenrechner.Classes
+{
+    class Kreditrechner
+    {
+        // zinsen
+        private static double Ratenkredit (double kredit, double laufzeit, double rate) {
+            double rest;
+            double monatsRate = rate / 12;
+            double interest = kredit * monatsRate;
+
+            double tilgungsAnteil = kredit / rate;
+
+            rest = kredit - tilgungsAnteil;
+
+            for (int i = 0; i < rest; i++) { 
+                    
+            }
+    }
+    }
+}
