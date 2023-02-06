2b55f5ea4254b976be4a669348b57bef027dc1f3
Nex0TV <julgroell@gmail.com>
2023-02-06 12:31:52 +0100

    jo

--- Classes/Schule.cs
+++ Classes/Schule.cs
@@ -19,5 +19,29 @@ namespace Taschenrechner.Classes
         {
             return (int)(durchschnitt + 0.5f);
         }
+
+        public static int[] ValidierteNoten (int[] noten)
+        {
+            int[] validierteNoten = new int[0];
+            foreach (int note in noten)
+            {
+                if(note <= 6 && note >= 1)
+                {
+                    Array.Resize<int>(ref validierteNoten, validierteNoten.Length + 1);
+                    validierteNoten[validierteNoten.Length - 1] = note;
+                }
+            }
+            return validierteNoten;
+        }
+
+        public static int Anzahl (int[] noten)
+        {
+            int anzahl = 0;
+            foreach (int note in noten)
+            {
+                anzahl++;
+            }
+            return anzahl;
+        }
     }
 }
