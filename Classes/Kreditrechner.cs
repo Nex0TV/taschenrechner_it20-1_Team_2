6a789e046e0432802555b7d5e15045b6c6ec94a8
Dang <dangvi>
2023-02-06 12:28:39 +0100

    changes and new class created

--- Classes/Kreditrechner.cs
+++ Classes/Kreditrechner.cs
@@ -7,11 +7,78 @@ using System.Windows.Forms;
 
 namespace Taschenrechner.Classes
 {
+    public class kredit
+    {
+        public bool OneTimePayment { get; set; }
+        public double Loan { get; set; }
+        public int Runtime { get; set; }
+        public double LoanRate { get; set; }
+        // constructor
+        public kredit(double loan, int runtime, double loanRate = 0, bool oneTimePayment = false)
+        {
+            this.OneTimePayment = oneTimePayment;
+            this.Loan = loan;
+            this.Runtime = runtime;
+            this.LoanRate = loanRate;
+
+            if (loanRate == 0)
+            {
+                this.LoanRate = loan / runtime;
+            }
+        }
+
+        public static double zinssatz(double zinssatz)
+        {
+            return zinssatz / 100;
+        }
+
+        public static double tilgung(double kreditsumme, double laufzeit)
+        {
+            return kreditsumme / laufzeit;
+        }
+        public static double laufzeitInMonaten(double dauerInJahren)
+        {
+            return dauerInJahren * 12;
+        }
+
+        public static double Zins(double zinssatz, double kreditsumme)
+        {
+            return kreditsumme * zinssatz;
+        }
+
+        public static double annuitaet(double tilgung, double zins)
+        {
+            return tilgung + zins;
+        }
+
+        public static double restKredit(double kreditsumme, double tilgung)
+        {
+            return kreditsumme - tilgung;
+        }
+
+        public static double gesamtZinsen(double zins, double dauer)
+        {
+            double gesamtZinsen = 0;
+
+            for (var i = 1; i < dauer; i++)
+            {
+                gesamtZinsen += zins;
+            }
+
+            return gesamtZinsen;
+        }
+
+        public double berechneKredit()
+        {
+
+        }
+    }
+
     public class Kreditrechner
     {
         // zinsen
         public static List<Dictionary<string, object>> Ratenkredit (double kreditsumme, int laufzeit, double zinssatz) {
-            List<Dictionary<string, object>> data = new List<Dictionary<string, object>>();
+           
 
             //var data = [arr, arr2];
             double kredit = kreditsumme;
@@ -47,7 +114,7 @@ namespace Taschenrechner.Classes
                 //    { "Gesamtbetrag", kredit },
                 //    { "Zins", zins },
                 //    { "Tilgung", tilgung },
-                //    { "Annuität", annuitaet },
+                //    { "Annuitaet", annuitaet },
                 //    { "Restschuld", kreditsumme },
                 //    { "Zinsen gesamt", gesamtZins },
                 //    { "Annuität gesamt", gesamtAnnuitaet }
@@ -57,58 +124,11 @@ namespace Taschenrechner.Classes
             }
 
 
-            foreach (Dictionary<string, object> entry in data)
-            {
-                Console.WriteLine(entry.Keys.Count);
-            }
-
-
-
             // Display the result on the form or in a data grid
-            return data;
+            //return data;
         }
 
-        public static double validateZinssatz(double zinssatz)
-        {
-
-            return zinssatz / 100;
-        }
-
-        public static double Tilgung(double kreditsumme, double laufzeit)
-        {
-            return kreditsumme / laufzeit;
-        }
-        public static double laufzeitInMonaten(double laufzeitInJahren)
-        {
-            return laufzeitInJahren * 12;
-        }
-
-        public static double Zins(double zinssatz, double kreditsumme)
-        {
-            return kreditsumme * zinssatz;
-        }
-
-        public static double annuitaet(double tilgung, double zins)
-        {
-            return tilgung + zins;
-        }
-
-        public static double restKredit(double kreditsumme, double tilgung)
-        {
-            return kreditsumme - tilgung;
-        }
-
-        public static double gesamtZinsen(double zins, double jahr)
-        {
-            double gesamtZinsen = 0;
-
-            for (var i = 1; i < jahr; i++)
-            {
-                 gesamtZinsen += zins;
-            }
-
-            return gesamtZinsen;
-        }
+       
     }
 }
 
