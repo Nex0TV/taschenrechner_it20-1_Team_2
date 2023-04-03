36f7a619d2f87f53ccf40d0ce165a6e695a8148f
Dang <dangvi>
2023-04-03 12:29:37 +0200

    changes xd

--- FormInfoTech.Designer.cs
+++ FormInfoTech.Designer.cs
@@ -35,25 +35,6 @@ namespace Taschenrechner
             this.btnInfoTechSquareRoot = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.btnInfoTechDelete = new System.Windows.Forms.Button();
-            this.btnPlus = new System.Windows.Forms.Button();
-            this.btnMinus = new System.Windows.Forms.Button();
-            this.btnDivision = new System.Windows.Forms.Button();
-            this.btnMultiply = new System.Windows.Forms.Button();
-            this.btnBackspace = new System.Windows.Forms.Button();
-            this.btnOpenBrackets = new System.Windows.Forms.Button();
-            this.btnNine = new System.Windows.Forms.Button();
-            this.btnEight = new System.Windows.Forms.Button();
-            this.btnSeven = new System.Windows.Forms.Button();
-            this.btnClosedBrackets = new System.Windows.Forms.Button();
-            this.btnSix = new System.Windows.Forms.Button();
-            this.btnFive = new System.Windows.Forms.Button();
-            this.btnFour = new System.Windows.Forms.Button();
-            this.btnSum = new System.Windows.Forms.Button();
-            this.btnThree = new System.Windows.Forms.Button();
-            this.btnTwo = new System.Windows.Forms.Button();
-            this.btnOne = new System.Windows.Forms.Button();
-            this.btnZero = new System.Windows.Forms.Button();
-            this.btnCommata = new System.Windows.Forms.Button();
             this.btnCloseBasicCalc = new System.Windows.Forms.Button();
             this.btnChooseModules = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -111,177 +92,6 @@ namespace Taschenrechner
             this.btnInfoTechDelete.Text = "Löschen";
             this.btnInfoTechDelete.UseVisualStyleBackColor = true;
             // 
-            // btnPlus
-            // 
-            this.btnPlus.Location = new System.Drawing.Point(16, 205);
-            this.btnPlus.Name = "btnPlus";
-            this.btnPlus.Size = new System.Drawing.Size(32, 29);
-            this.btnPlus.TabIndex = 6;
-            this.btnPlus.Text = "+";
-            this.btnPlus.UseVisualStyleBackColor = true;
-            // 
-            // btnMinus
-            // 
-            this.btnMinus.Location = new System.Drawing.Point(54, 205);
-            this.btnMinus.Name = "btnMinus";
-            this.btnMinus.Size = new System.Drawing.Size(32, 29);
-            this.btnMinus.TabIndex = 7;
-            this.btnMinus.Text = "-";
-            this.btnMinus.UseVisualStyleBackColor = true;
-            // 
-            // btnDivision
-            // 
-            this.btnDivision.Location = new System.Drawing.Point(130, 205);
-            this.btnDivision.Name = "btnDivision";
-            this.btnDivision.Size = new System.Drawing.Size(32, 29);
-            this.btnDivision.TabIndex = 9;
-            this.btnDivision.Text = "/";
-            this.btnDivision.UseVisualStyleBackColor = true;
-            // 
-            // btnMultiply
-            // 
-            this.btnMultiply.Location = new System.Drawing.Point(92, 205);
-            this.btnMultiply.Name = "btnMultiply";
-            this.btnMultiply.Size = new System.Drawing.Size(32, 29);
-            this.btnMultiply.TabIndex = 8;
-            this.btnMultiply.Text = "*";
-            this.btnMultiply.UseVisualStyleBackColor = true;
-            // 
-            // btnBackspace
-            // 
-            this.btnBackspace.Location = new System.Drawing.Point(168, 205);
-            this.btnBackspace.Name = "btnBackspace";
-            this.btnBackspace.Size = new System.Drawing.Size(32, 29);
-            this.btnBackspace.TabIndex = 10;
-            this.btnBackspace.Text = "<-";
-            this.btnBackspace.UseVisualStyleBackColor = true;
-            // 
-            // btnOpenBrackets
-            // 
-            this.btnOpenBrackets.Location = new System.Drawing.Point(130, 240);
-            this.btnOpenBrackets.Name = "btnOpenBrackets";
-            this.btnOpenBrackets.Size = new System.Drawing.Size(32, 29);
-            this.btnOpenBrackets.TabIndex = 14;
-            this.btnOpenBrackets.Text = "(";
-            this.btnOpenBrackets.UseVisualStyleBackColor = true;
-            // 
-            // btnNine
-            // 
-            this.btnNine.Location = new System.Drawing.Point(92, 240);
-            this.btnNine.Name = "btnNine";
-            this.btnNine.Size = new System.Drawing.Size(32, 29);
-            this.btnNine.TabIndex = 13;
-            this.btnNine.Text = "9";
-            this.btnNine.UseVisualStyleBackColor = true;
-            // 
-            // btnEight
-            // 
-            this.btnEight.Location = new System.Drawing.Point(54, 240);
-            this.btnEight.Name = "btnEight";
-            this.btnEight.Size = new System.Drawing.Size(32, 29);
-            this.btnEight.TabIndex = 12;
-            this.btnEight.Text = "8";
-            this.btnEight.UseVisualStyleBackColor = true;
-            // 
-            // btnSeven
-            // 
-            this.btnSeven.Location = new System.Drawing.Point(16, 240);
-            this.btnSeven.Name = "btnSeven";
-            this.btnSeven.Size = new System.Drawing.Size(32, 29);
-            this.btnSeven.TabIndex = 11;
-            this.btnSeven.Text = "7";
-            this.btnSeven.UseVisualStyleBackColor = true;
-            // 
-            // btnClosedBrackets
-            // 
-            this.btnClosedBrackets.Location = new System.Drawing.Point(130, 275);
-            this.btnClosedBrackets.Name = "btnClosedBrackets";
-            this.btnClosedBrackets.Size = new System.Drawing.Size(32, 29);
-            this.btnClosedBrackets.TabIndex = 18;
-            this.btnClosedBrackets.Text = ")";
-            this.btnClosedBrackets.UseVisualStyleBackColor = true;
-            // 
-            // btnSix
-            // 
-            this.btnSix.Location = new System.Drawing.Point(92, 275);
-            this.btnSix.Name = "btnSix";
-            this.btnSix.Size = new System.Drawing.Size(32, 29);
-            this.btnSix.TabIndex = 17;
-            this.btnSix.Text = "6";
-            this.btnSix.UseVisualStyleBackColor = true;
-            // 
-            // btnFive
-            // 
-            this.btnFive.Location = new System.Drawing.Point(54, 275);
-            this.btnFive.Name = "btnFive";
-            this.btnFive.Size = new System.Drawing.Size(32, 29);
-            this.btnFive.TabIndex = 16;
-            this.btnFive.Text = "5";
-            this.btnFive.UseVisualStyleBackColor = true;
-            // 
-            // btnFour
-            // 
-            this.btnFour.Location = new System.Drawing.Point(16, 275);
-            this.btnFour.Name = "btnFour";
-            this.btnFour.Size = new System.Drawing.Size(32, 29);
-            this.btnFour.TabIndex = 15;
-            this.btnFour.Text = "4";
-            this.btnFour.UseVisualStyleBackColor = true;
-            // 
-            // btnSum
-            // 
-            this.btnSum.Location = new System.Drawing.Point(130, 310);
-            this.btnSum.Name = "btnSum";
-            this.btnSum.Size = new System.Drawing.Size(32, 64);
-            this.btnSum.TabIndex = 22;
-            this.btnSum.Text = "=";
-            this.btnSum.UseVisualStyleBackColor = true;
-            // 
-            // btnThree
-            // 
-            this.btnThree.Location = new System.Drawing.Point(92, 310);
-            this.btnThree.Name = "btnThree";
-            this.btnThree.Size = new System.Drawing.Size(32, 29);
-            this.btnThree.TabIndex = 21;
-            this.btnThree.Text = "3";
-            this.btnThree.UseVisualStyleBackColor = true;
-            // 
-            // btnTwo
-            // 
-            this.btnTwo.Location = new System.Drawing.Point(54, 310);
-            this.btnTwo.Name = "btnTwo";
-            this.btnTwo.Size = new System.Drawing.Size(32, 29);
-            this.btnTwo.TabIndex = 20;
-            this.btnTwo.Text = "2";
-            this.btnTwo.UseVisualStyleBackColor = true;
-            // 
-            // btnOne
-            // 
-            this.btnOne.Location = new System.Drawing.Point(16, 310);
-            this.btnOne.Name = "btnOne";
-            this.btnOne.Size = new System.Drawing.Size(32, 29);
-            this.btnOne.TabIndex = 19;
-            this.btnOne.Text = "1";
-            this.btnOne.UseVisualStyleBackColor = true;
-            // 
-            // btnZero
-            // 
-            this.btnZero.Location = new System.Drawing.Point(16, 345);
-            this.btnZero.Name = "btnZero";
-            this.btnZero.Size = new System.Drawing.Size(70, 29);
-            this.btnZero.TabIndex = 19;
-            this.btnZero.Text = "0";
-            this.btnZero.UseVisualStyleBackColor = true;
-            // 
-            // btnCommata
-            // 
-            this.btnCommata.Location = new System.Drawing.Point(92, 345);
-            this.btnCommata.Name = "btnCommata";
-            this.btnCommata.Size = new System.Drawing.Size(32, 29);
-            this.btnCommata.TabIndex = 22;
-            this.btnCommata.Text = ",";
-            this.btnCommata.UseVisualStyleBackColor = true;
-            // 
             // btnCloseBasicCalc
             // 
             this.btnCloseBasicCalc.Location = new System.Drawing.Point(168, 402);
@@ -307,25 +117,6 @@ namespace Taschenrechner
             this.ClientSize = new System.Drawing.Size(316, 450);
             this.Controls.Add(this.btnCloseBasicCalc);
             this.Controls.Add(this.btnChooseModules);
-            this.Controls.Add(this.btnCommata);
-            this.Controls.Add(this.btnSum);
-            this.Controls.Add(this.btnThree);
-            this.Controls.Add(this.btnZero);
-            this.Controls.Add(this.btnTwo);
-            this.Controls.Add(this.btnOne);
-            this.Controls.Add(this.btnClosedBrackets);
-            this.Controls.Add(this.btnSix);
-            this.Controls.Add(this.btnFive);
-            this.Controls.Add(this.btnFour);
-            this.Controls.Add(this.btnOpenBrackets);
-            this.Controls.Add(this.btnNine);
-            this.Controls.Add(this.btnEight);
-            this.Controls.Add(this.btnSeven);
-            this.Controls.Add(this.btnBackspace);
-            this.Controls.Add(this.btnDivision);
-            this.Controls.Add(this.btnMultiply);
-            this.Controls.Add(this.btnMinus);
-            this.Controls.Add(this.btnPlus);
             this.Controls.Add(this.btnInfoTechDelete);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.btnInfoTechSquareRoot);
@@ -348,25 +139,6 @@ namespace Taschenrechner
         private System.Windows.Forms.Button btnInfoTechSquareRoot;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button btnInfoTechDelete;
-        private System.Windows.Forms.Button btnPlus;
-        private System.Windows.Forms.Button btnMinus;
-        private System.Windows.Forms.Button btnDivision;
-        private System.Windows.Forms.Button btnMultiply;
-        private System.Windows.Forms.Button btnBackspace;
-        private System.Windows.Forms.Button btnOpenBrackets;
-        private System.Windows.Forms.Button btnNine;
-        private System.Windows.Forms.Button btnEight;
-        private System.Windows.Forms.Button btnSeven;
-        private System.Windows.Forms.Button btnClosedBrackets;
-        private System.Windows.Forms.Button btnSix;
-        private System.Windows.Forms.Button btnFive;
-        private System.Windows.Forms.Button btnFour;
-        private System.Windows.Forms.Button btnSum;
-        private System.Windows.Forms.Button btnThree;
-        private System.Windows.Forms.Button btnTwo;
-        private System.Windows.Forms.Button btnOne;
-        private System.Windows.Forms.Button btnZero;
-        private System.Windows.Forms.Button btnCommata;
         private System.Windows.Forms.Button btnCloseBasicCalc;
         private System.Windows.Forms.Button btnChooseModules;
     }
