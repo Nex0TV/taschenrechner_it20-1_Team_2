9752f94f02dcef67fcfafdc481af594313b75bf8
Dang <dangvi>
2023-04-04 11:18:56 +0200

    fuxing loan changes

--- Classes/Kreditrechner.cs
+++ Classes/Kreditrechner.cs
@@ -9,96 +9,125 @@ namespace Taschenrechner.Classes
 {
     public class kredit
     {
-        public bool OneTimePayment { get; set; }
-        public double Loan { get; set; }
-        public int Runtime { get; set; }
-        public double LoanRate { get; set; }
-        // constructor
-        public kredit(double loan, int runtime, double loanRate = 0, bool oneTimePayment = false)
+        //Ratenkredit mit Vorgabe Ratenhöhe
+        private double zins(double betrag, double zinssatz, int laufzeit)
         {
-            this.OneTimePayment = oneTimePayment;
-            this.Loan = loan;
-            this.Runtime = runtime;
-            this.LoanRate = loanRate;
-
-            if (loanRate == 0)
+            double zinsen;
+            double faktor = 0;
+            for (int i = 1; i <= laufzeit; i++)
             {
-                this.LoanRate = loan / runtime;
+                faktor = faktor + i;
             }
+            faktor = faktor / laufzeit;
 
+            zinsen = betrag * faktor * zinssatz / 100 / 12;
+            return zinsen;
 
         }
 
-        public static double zinssatz(double zinssatz)
+        private string berechneKredit(double kredit, double zinssatz, double rate)
         {
-            return zinssatz / 100;
+            double laufzeit = kredit / rate;
+            laufzeit = Math.Ceiling(laufzeit);
+
+            double zinsen = zins(kredit, zinssatz, (int)laufzeit);
+            laufzeit = Math.Ceiling((kredit + zinsen) / rate);
+            double schlussrate = Math.Ceiling(kredit + zinsen) - (laufzeit - 1) * rate;
+
+            string ergebnis = String.Format("Kredit: {0:f} €, Zinssatz: {1:f} % Rate: {2:f} € -> Laufzeit: {3:d} Monate, Schlussrate: {4:f} €", kredit,zinssatz, rate, Convert.ToInt32(laufzeit), schlussrate);
+            return ergebnis;
         }
 
-        public static double tilgung(double kreditsumme, double laufzeit)
-        {
-            return kreditsumme / laufzeit;
-        }
-        public static double laufzeitInMonaten(double dauerInJahren)
-        {
-            return dauerInJahren * 12;
-        }
+        //    public bool OneTimePayment { get; set; }
+        //    public double Loan { get; set; }
+        //    public int Runtime { get; set; }
+        //    public double LoanRate { get; set; }
+        //    // constructor
+        //    public kredit(double loan, int runtime, double loanRate = 0, bool oneTimePayment = false)
+        //    {
+        //        this.OneTimePayment = oneTimePayment;
+        //        this.Loan = loan;
+        //        this.Runtime = runtime;
+        //        this.LoanRate = loanRate;
 
-        public static double Zins(double zinssatz, double kreditsumme)
-        {
-            return kreditsumme * zinssatz;
-        }
+        //        if (loanRate == 0)
+        //        {
+        //            this.LoanRate = loan / runtime;
+        //        }
 
-        public static double annuitaet(double tilgung, double zins)
-        {
-            return tilgung + zins;
-        }
 
-        public static double restKredit(double kreditsumme, double tilgung)
-        {
-            return kreditsumme - tilgung;
-        }
+        //    }
 
-        public static double gesamtZinsen(double kreditsumme, double zins, double dauerMonate, double _tilgung)
-        {
-            double gesamtZinsen = 0;
+        //    public static double zinssatz(double zinssatz)
+        //    {
+        //        return zinssatz / 100;
+        //    }
 
-            for (var i = 1; i < dauerMonate; i++)
-            {
-                gesamtZinsen += kreditsumme * zins / 12;
-                kreditsumme -= _tilgung;
-            }
+        //    public static double tilgung(double kreditsumme, double laufzeit)
+        //    {
+        //        return kreditsumme / laufzeit;
+        //    }
+        //    public static double laufzeitInMonaten(double dauerInJahren)
+        //    {
+        //        return dauerInJahren * 12;
+        //    }
 
-            return gesamtZinsen;
-        }
+        //    public static double Zins(double zinssatz, double kreditsumme)
+        //    {
+        //        return kreditsumme * zinssatz;
+        //    }
 
-        public static Dictionary<string, object> berechneKredit(double kreditsumme, double laufzeitMonate, double zinssatz)
-        {
-            Dictionary<string, object> data = new Dictionary<string, object>();
-            double _tilgung = tilgung(kreditsumme, laufzeitMonate);
-            double kredit = kreditsumme;
+        //    public static double annuitaet(double tilgung, double zins)
+        //    {
+        //        return tilgung + zins;
+        //    }
 
-            for (int i = 0; i < laufzeitMonate; i++)
-            {
-                double zins = Zins(kreditsumme, zinssatz);
+        //    public static double restKredit(double kreditsumme, double tilgung)
+        //    {
+        //        return kreditsumme - tilgung;
+        //    }
 
-                double _annuitaet = annuitaet(_tilgung, zins);
+        //    public static double gesamtZinsen(double kreditsumme, double zins, double dauerMonate, double _tilgung)
+        //    {
+        //        double gesamtZinsen = 0;
 
-                kreditsumme = restKredit(kreditsumme, _tilgung);
+        //        for (var i = 1; i < dauerMonate; i++)
+        //        {
+        //            gesamtZinsen += kreditsumme * zins / 12;
+        //            kreditsumme -= _tilgung;
+        //        }
 
-                double gesamtZins = gesamtZinsen(kreditsumme, zins, laufzeitMonate, _tilgung);
+        //        return gesamtZinsen;
+        //    }
 
-                data.Add("Gesamtbetrag", kredit);
-                data.Add("Zins", zins);
-                data.Add("Tilgung", _tilgung);
-                data.Add("Annuitaet", _annuitaet);
-                data.Add("Restschuld", kreditsumme );
-                data.Add("Zinsen gesamt", gesamtZins);
-            }
-            Console.WriteLine(data);
-            return data;
-        }
+        //    public static Dictionary<string, object> berechneKredit(double kreditsumme, double laufzeitMonate, double zinssatz)
+        //    {
+        //        Dictionary<string, object> data = new Dictionary<string, object>();
+        //        double _tilgung = tilgung(kreditsumme, laufzeitMonate);
+        //        double kredit = kreditsumme;
+
+        //        for (int i = 0; i < laufzeitMonate; i++)
+        //        {
+        //            double zins = Zins(kreditsumme, zinssatz);
+
+        //            double _annuitaet = annuitaet(_tilgung, zins);
+
+        //            kreditsumme = restKredit(kreditsumme, _tilgung);
+
+        //            double gesamtZins = gesamtZinsen(kreditsumme, zins, laufzeitMonate, _tilgung);
+
+        //            data.Add("Gesamtbetrag", kredit);
+        //            data.Add("Zins", zins);
+        //            data.Add("Tilgung", _tilgung);
+        //            data.Add("Annuitaet", _annuitaet);
+        //            data.Add("Restschuld", kreditsumme );
+        //            data.Add("Zinsen gesamt", gesamtZins);
+        //        }
+        //        Console.WriteLine(data);
+        //        return data;
+        //    }
+        //}
     }
-}
 
 
 
