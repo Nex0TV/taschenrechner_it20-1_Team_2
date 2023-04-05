4e57eb10f4a1d02e93555701c9735f7ab95400f7
Dang <dangvi>
2023-04-05 07:54:06 +0200

    fuaqin loan calc works now

--- TaschenrechnerUnitTests/Kredit/Berechnung.cs
+++ TaschenrechnerUnitTests/Kredit/Berechnung.cs
@@ -11,7 +11,7 @@ namespace TaschenrechnerUnitTests.Kredit {
         [Test]
         public void Berechnen()
         {
-            //kredit.RatenkreditVorgabeRatenhoehe(1000, 3.5, 502.19);
+            kredit.RatenkreditVorgabeRatenhoehe(1000, 3.5, 502.19);
             kredit.RatenkreditVorgabeLaufzeit(1000, 3.5, 1);
         }
     }
