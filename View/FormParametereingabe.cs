ca1d88ede02dbe58571488bffd331bc7f121282d
Fabius Grüner <fabiusgruener@gmx.de>
2023-04-17 14:57:46 +0200

    Bugfix and refactor

--- View/FormParametereingabe.cs
+++ View/FormParametereingabe.cs
@@ -26,64 +26,73 @@ namespace Taschenrechner.View
             Close();
         }
 
+        private void add_character(object sender)
+        {
+            Button btn = (Button)sender;
+            this.tbox_result.Text += btn.Text;
+        }
+
         private void btn_1_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "1";
+            add_character(sender);
         }
 
         private void btn_2_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "2";
+            add_character(sender);
         }
 
         private void btn_3_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "3";
+            add_character(sender);
         }
 
         private void btn_4_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "4";
+            add_character(sender);
         }
 
         private void btn_5_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "5";
+            add_character(sender);
         }
 
         private void btn_6_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "6";
+            add_character(sender);
         }
 
         private void btn_7_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "7";
+            add_character(sender);
         }
 
         private void btn_8_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "8";
+            add_character(sender);
         }
 
         private void btn_9_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "9";
+            add_character(sender);
         }
 
         private void btn_0_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += "0";
+            add_character(sender);
         }
 
         private void btn_dot_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text += ".";
+            add_character(sender);
         }
 
         private void btn_back_Click(object sender, EventArgs e)
         {
-            this.tbox_result.Text = this.tbox_result.Text.Substring(0, this.tbox_result.Text.Length - 1);
+            if (this.tbox_result.Text.Length > 0)
+            {
+                this.tbox_result.Text = this.tbox_result.Text.Substring(0, this.tbox_result.Text.Length - 1);
+            }
         }
 
         private void btn_nebenrechnung_Click(object sender, EventArgs e)
